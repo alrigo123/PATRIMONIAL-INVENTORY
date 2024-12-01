f027534320bf24125785fc753a1bafc97c3d8ce5
alrigo123 <alexestrada2804@gmail.com>
2024-12-01 07:07:05 -0500

    REGISTER

---
+++ server/controllers/userManagement.controller.js
@@ -0,0 +1,13 @@
+import pool from '../db.js';
+
+export const getUser = async (req, res, next) => {
+    try {
+    } catch (error) {
+    }
+};
+
+export const registerUser = async (req, res, next) => {
+    try {
+    } catch (error) {
+    }
+};
---
+++ server/routes/user.routes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+import {} from "../controllers/exportReports.controller.js";
+
+
+const router = Router();
+
+//EXPORT DATA TO EXCEL
+// router.get("/state", getItemsGeneralState);
+
+export default router;
